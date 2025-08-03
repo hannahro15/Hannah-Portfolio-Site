318d44482c39ad0b2a63e27f6319a784744ff9fb
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-03 21:05:19 +0100

    Fix initial setup issues and add home page link

---
+++ hannah-portfolio/src/pages/Home.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Home() {
+    return (
+        <div className="App">
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the home page of my portfolio site.</p>
+        </div>
+    );
+}
+
+export default Home;
