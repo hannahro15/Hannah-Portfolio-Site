f5208c4ed478e2ebefaa8b61e0468a2749f29066
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-05 18:53:53 +0100

    Add a basic navbar

---
+++ hannah-portfolio/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
--- hannah-portfolio/src/pages/Home.js
+++ hannah-portfolio/src/pages/Home.js
@@ -1,11 +1,15 @@
 import '../App.css';
+import Navbar from '../components/Navbar';
 
 function Home() {
     return (
-        <div className="App">
-            <h1>Welcome to My Portfolio</h1>
-            <p>This is the home page of my portfolio site.</p>
-        </div>
+        <>
+            <Navbar />
+            <div className="App">
+                <h1>Welcome to My Portfolio</h1>
+                <p>This is the home page of my portfolio site.</p>
+            </div>
+        </>
     );
 }
 
