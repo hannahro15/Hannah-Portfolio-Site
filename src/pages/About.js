8d4d333e36d9eeec093a4e1bc7db5856a3aa472a
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-22 16:29:30 +0100

    Add content for the about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,14 @@ function About() {
         <div className="about">
             <Navbar />
             <h1>About Me</h1>
-            <p>This is the about page.</p>
+            <p>I am mainly a self-taught developer originally coming from a musical background. I have gone through various online courses and bootcamps to enhance my skills.
+                My non-technical experience in music teaching, retail, customer service, libraries and administration.
+            </p>
+            <h2>Technical Skills</h2>
+            <p>HTML, CSS, JavaScript, React, SQL, and Python. I also have experience with Git, GitHub, and Agile methodologies and databases like MongoDB.</p>
+            <h2>Hobbies & Interests</h2>
+            <p>In my free time, I enjoy working on personal coding projects, playing in amateur orchestras, cooking, baking, and doing jigsaw puzzles.
+            </p>
         </div>
     );
 }
