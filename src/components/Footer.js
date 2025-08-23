5ed02d4bbc8bf7047c43ccd627720288df171a20
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-23 16:10:50 +0100

    Remove extra classes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <a href="https://www.linkedin.com/in/hannaholbrich/" className="fa fa-linkedin" aria-label="LinkedIn Profile">
+            <a href="https://www.linkedin.com/in/hannaholbrich/" aria-label="LinkedIn Profile">
                 <i className="fab fa-linkedin"></i>
             </a>
-            <a href="https://github.com/hannahro15" className="fa fa-github" aria-label="GitHub Profile">
+            <a href="https://github.com/hannahro15" aria-label="GitHub Profile">
                 <i className="fab fa-github"></i>
             </a>
             <p>&copy; 2025 Hannah Olbrich. All rights reserved.</p>
