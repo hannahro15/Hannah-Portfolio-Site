9e627822010798ff568e6726963540576f6acab3
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-23 16:02:45 +0100

    Fix footer accessibility issues for successful deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,12 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <a href="https://www.linkedin.com/in/hannaholbrich/" className="fa fa-linkedin"></a>
-            <a href="https://github.com/hannahro15" className="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/hannaholbrich/" className="fa fa-linkedin" aria-label="LinkedIn Profile">
+                <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/hannahro15" className="fa fa-github" aria-label="GitHub Profile">
+                <i className="fab fa-github"></i>
+            </a>
             <p>&copy; 2025 Hannah Olbrich. All rights reserved.</p>
         </footer>
     );
