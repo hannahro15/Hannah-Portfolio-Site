6ff5c1689197aebf3f1c2cd75f42dd8c0533dd1b
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-23 15:07:43 +0100

    Make background image on all pages and amend text color to white

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import backgroundImg from './assets/background-img.jpg';
 
 function App() {
+  const appStyle = {
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    minHeight: '100vh'
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
+    <div style={appStyle}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,12 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import backgroundImg from '../assets/background-img.jpg';
 import '../pages/Home.css'
 
 function Home() {
-    const homeStyle = {
-        backgroundImage: `url(${backgroundImg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        minHeight: '100vh'
-    };
-
     return (
         <>
             <Navbar />
-            <div className="home" style={homeStyle}>
+            <div className="home">
                 <h1>Welcome to Hannah's Portfolio</h1>
                 <h2>Junior Web Developer and Data Professional</h2>
                 <p>Click <Link to="/projects"> here </Link>to view my solo and hackathon projects.</p>
