6f9272737e1a05a7d91312467c35db580b98eb32
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-23 14:55:13 +0100

    Remove contact link from Navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
   )
 }
