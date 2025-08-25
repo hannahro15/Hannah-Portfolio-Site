7d9323b805ea0da3c73dbda82ddea617069c57e8
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-25 23:34:10 +0100

    Replace link with link with navlink in navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/projects">Projects</NavLink></li>
             </ul>
         </nav>
     );
