57b8d6e8e4de4b076a75562b8c7b461b0571cee5
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-27 11:14:43 +0100

    Add profile pic to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import '../pages/Home.css'
 function Home() {
     return (
         <div className="home">
+            <img src={require('../assets/profile-pic.jpg')} alt="Profile pic of myself" className="profile-pic" />
             <h1>Welcome to Hannah's Portfolio</h1>
             <h2>Junior Web Developer and Data Professional</h2>
             <p>Click <Link to="/projects"> here </Link>to view my solo and hackathon projects.</p>
