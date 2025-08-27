0f487814d6031ff88130ff78de7c76629d2d5739
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-08-27 11:17:33 +0100

    Change require to an proper import so profile is show

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
-import '../pages/Home.css'
+import '../pages/Home.css';
+import profilePic from '../assets/profile-pic.jpg';
 
 function Home() {
     return (
         <div className="home">
-            <img src={require('../assets/profile-pic.jpg')} alt="Profile pic of myself" className="profile-pic" />
+            <img src={profilePic} alt="Profile pic of myself" className="profile-pic" />
             <h1>Welcome to Hannah's Portfolio</h1>
             <h2>Junior Web Developer and Data Professional</h2>
             <p>Click <Link to="/projects"> here </Link>to view my solo and hackathon projects.</p>
