d6f4b2f9a74ca1a348f640fdf107937c10112e6b
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-05 16:56:11 +0100

    Add in a basic bootstrap contact form to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,27 @@
 import './Contact.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Contact() {
     return (
         <div className="contact">
             <h1>Contact Me</h1>
-            <p>If you wish to contact me please use the form below. Thanks a lot.</p>
-            <form
-                action="https://formspree.io/f/xjkeydor"
-                method="POST">
-                <label>
-                    Your name:
-                    <input id="name" type="text" name="name" required />
-                </label>
-                <label>
-                    Your email:
-                    <input id="email" type="email" name="email" required />
-                </label>
-                <label>
-                    Your message:
-                    <textarea id="message" name="message" required></textarea>
-                </label>
-                <button id="submit" type="submit">Send Message</button>
-            </form>
+            <p>If you wish to contact me, please use the form below. Thanks a lot.</p>
+            <Form className="contact-form" action="https://formspree.io/f/xjkeydor" method="POST">
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Full Name" name="name" required />
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" name="email" required />
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" rows={5} name="message" required />
+                </Form.Group>
+                <Button className="w-60 text-center" type="submit">Submit form</Button>
+            </Form>
         </div>
-    );
-}
+    )
+};
+
+
 
 export default Contact;
