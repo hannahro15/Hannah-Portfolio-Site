c1843e329a7660ced3ce9978e3925d08c6487eee
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-10 23:52:33 +0100

    Improve project descriptions

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,35 +2,35 @@
 export const solo_projects = [
     {
         title: "Personal Portfolio",
-        description: "My personal portfolio site built with React.",
+        description: "My personal portfolio site built with React showcasing my projects and information about myself and skills.",
         technologies: ["React, ", "JavaScript, ", "HTML, ", "CSS "],
         github: "https://github.com/hannahro15/hannah-portfolio-site",
         demo: "https://hannahro15.github.io/Hannah-Portfolio-Site/"
     },
     {
         title: "Ready, Steady, Eat",
-        description: "A fake e-commerce food delivery store",
+        description: "A fake e-commerce food delivery platform featuring user authentication system, Stripe payments and order management.",
         technologies: ["HTML, ", "CSS, ", "JavaScript, ", "JQuery, ", "Bootstrap, ", "Python, ", "Django "],
         github: "https://github.com/hannahro15/ready-steady-eat",
         demo: "https://ready-steady-eat-8febfd678f9f.herokuapp.com/"
     },
     {
         title: "Theme Parks Ride Reviews Site",
-        description: "A theme park ride reviews site based on a few theme parks in the UK.",
+        description: "A theme park ride reviews site based on a few theme parks in the UK where users can write reviews on rides.",
         technologies: ["HTML, ", "CSS, ", "Bootstrap, ", "Python, ", "Flask, ", "MongoDB "],
         github: "https://github.com/hannahro15/Theme-park-ride-reviews-site",
         demo: "https://theme-park-reviews-website-f1f235eaa19d.herokuapp.com/"
     },
     {
         title: "Manchester Tourist Site",
-        description: "A site for Manchester tourists with featuring a memory game on it.",
+        description: "A site for Manchester tourists featuring a memory game and information about things to do around Manchester.",
         technologies: ["HTML, ", "CSS, ", "Bootstrap, ", "JavaScript "],
         github: "https://github.com/hannahro15/Manchester-Tourist-Project",
         demo: "https://hannahro15.github.io/Manchester-Tourist-Project/"
     },
     {
         title: "Manchester Cattery",
-        description: "A fake cattery business based in Manchester (that doesn't exist).",
+        description: "A fake cattery business website based in Manchester showing services, contact forms and responsive design.",
         technologies: ["HTML, ", "CSS, ", "Bootstrap "],
         github: "https://github.com/hannahro15/Cattery-project",
         demo: "https://hannahro15.github.io/Cattery-project/"
@@ -47,7 +47,7 @@ export const hack_projects = [
     },
     {
         title: "Script to Sound",
-        description: "A text to speech appp built as part of the accessibility hackathon.",
+        description: "A text to speech app built as part of the accessibility hackathon.",
         technologies: ["HTML, ", "CSS, ", "JavaScript "],
         github: "https://github.com/hannahro15/Nov24Hackathon",
         demo: "https://script-to-sound-f697c5a0720c.herokuapp.com/"
