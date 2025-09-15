0e26648b07a801883a3ee9bd6e91d75ae42820bc
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-15 13:26:14 +0100

    Fix typo on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 
 function Contact() {
     return (
-        <div className="contact text-center'">
+        <div className="contact text-center">
             <h1 className="text-center">Contact Me</h1>
             <p className="text-center">If you wish to contact me, please use the form below. Thanks a lot.</p>
             <Form className="contact-form" action="https://formspree.io/f/xjkeydor" method="POST">
