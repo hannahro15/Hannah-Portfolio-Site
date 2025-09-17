3b67a308c15e8bf4a414b246918edf40493a0e4e
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-17 21:16:36 +0100

    Add more content to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,11 +13,11 @@ function About() {
                 <li><strong>Frontend: </strong>HTML, CSS, JavaScript, React, Bootstrap, Tailwind, Materialize</li>
                 <li><strong>Backend & Databases: </strong>Python, Django, Flask, SQL, MongoDB</li>
                 <li><strong>Data & Analysis: </strong>Pandas, Numpy, Matplotlib, Seaborn</li>
-                <li><strong>Tools & Methodologies: </strong>Git, GitHub, Heroku, AWS S3, AWS EC2, Agile, Scrum</li>
+                <li><strong>Tools & Methodologies: </strong>Git, GitHub, Heroku, AWS S3, AWS EC2, Agile, Scrum, Docker</li>
             </ul>
             <hr></hr>
             <h2>Hobbies & Interests</h2>
-            <p>In my free time, I enjoy working on personal coding projects, playing in amateur orchestras, cooking, baking, and doing jigsaw puzzles.
+            <p>In my free time, I enjoy working on personal coding projects, playing in amateur orchestras, cooking, baking, and doing jigsaw puzzles. I also like gardening, and occasionally going to theme parks and escape rooms.
             </p>
         </main>
     );
