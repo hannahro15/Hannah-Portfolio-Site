4141af2b7bc6be72b5608a056634b2e31bfc0538
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-17 21:03:24 +0100

    Add tagline to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ function Home() {
             <img src={profilePic} alt="Hannah Olbrich, Web Developer" className="profile-pic" />
             <h2>Junior Web Developer & Data Professional</h2>
             <p>Seeking opportunities in Manchester/Remote</p>
+            <p><em>Passionate web developer with a musical background, delivering creative apps and websites.</em></p>
             <div className="cta-buttons">
                 <Link to="/projects" className="btn btn-secondary">View My Projects</Link>
                 <Link to="/about" className="btn btn-secondary">About Me</Link>
