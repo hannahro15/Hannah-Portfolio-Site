73fe4d2d56c0060ff0cacf324ba9a3b87a8b5f36
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-29 10:23:10 +0100

    Minor tweaks to layout and content on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,9 @@ function Home() {
     return (
         <main className="home">
             <img src={profilePic} alt="Hannah Olbrich, Web Developer" className="profile-pic" />
-            <h2>Junior Web Developer & Data Professional</h2>
-            <p>Seeking opportunities in Manchester/Remote</p>
+            <h1>Junior Software Developer</h1>
             <p><em>Passionate web developer with a musical background, delivering creative apps and websites.</em></p>
+            <p>Seeking opportunities in Manchester/Remote</p>
             <div className="cta-buttons">
                 <Link to="/projects" className="btn btn-secondary">View My Projects</Link>
                 <Link to="/about" className="btn btn-secondary">About Me</Link>
