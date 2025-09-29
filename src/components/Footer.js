c3e17a96138ba1c3ec978894548f83c1ae63894a
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-29 10:19:26 +0100

    Add accessibility elements to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="social-links">
-                <a href="https://www.linkedin.com/in/hannaholbrich/" aria-label="LinkedIn Profile">
+                <a href="https://www.linkedin.com/in/hannaholbrich/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
                     <i className="fab fa-linkedin"></i>
                 </a>
-                <a href="https://github.com/hannahro15" aria-label="GitHub Profile">
+                <a href="https://github.com/hannahro15" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile">
                     <i className="fab fa-github"></i>
                 </a>
             </div>
