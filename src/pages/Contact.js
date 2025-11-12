f86c1d351a992b020b793e565aabebf8000afc18
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-12 21:15:09 +0000

    Add aria labels to contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,19 +8,19 @@ function Contact() {
             <h1 className="text-center">Contact Me</h1>
             <p className="text-center">If you wish to contact me, please use the form below. Thanks a lot.</p>
             <Form className="contact-form" action="https://formspree.io/f/xjkeydor" method="POST">
-                <Form.Group className="mb-3" controlId="name">
+                <Form.Group className="mb-3" controlId="name" aria-label="Enter your full name">
                     <Form.Label>Full Name</Form.Label>
                     <Form.Control type="text" placeholder="Full Name" name="name" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="email">
+                <Form.Group className="mb-3" controlId="email" aria-label="Enter your email address">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" name="email" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="message">
+                <Form.Group className="mb-3" controlId="message" aria-label="Enter your message below">
                     <Form.Label>Message</Form.Label>
                     <Form.Control as="textarea" rows={5} name="message" placeholder="Please enter your message here" required />
                 </Form.Group>
-                <Button className="w-60 btn-primary" type="submit">Submit form</Button>
+                <Button className="w-60 btn-primary" type="submit" aria-label="Submit contact form">Submit form</Button>
             </Form>
         </main>
     )
