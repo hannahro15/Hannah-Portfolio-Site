51618aa153f1fc3ef8db59761f8ca85a1c02e763
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-13 16:45:17 +0000

    Add pills to the tech icons

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,10 +5,16 @@ function ProjectCard({ project }) {
         <div className="project-card text-center">
             <h3 id='project-title'><strong>{project.title}</strong></h3>
             {project.image && (
-                <img src={project.image} alt={project.title} className="project-image" />
+                <img src={project.image} alt={`Screenshot of ${project.title}`} className="project-image" />
             )}
             <p><strong>Description: </strong>{project.description}</p>
-            <p><strong>Technologies: </strong>{project.technologies}</p>
+             <div className="tech-badges">
+                    {project.technologies.map((tech, index) => (
+                        <span key={index} className="badge bg-secondary me-2 mb-2">
+                            {tech.replace(',', '').trim()}
+                        </span>
+                    ))}
+            </div>   
             <a className="github-link" href={project.github} target="_blank" rel="noreferrer">GitHub</a>
             <br />
             <a className="live-site-link" href={project.demo} target="_blank" rel="noreferrer">Live Site</a>
